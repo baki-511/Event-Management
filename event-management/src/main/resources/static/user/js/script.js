dc0ff5b79f10e9623de400fbe960ac1ae5e75367
baki-511 <johanbaki381@gmail.com>
2024-12-24 07:07:29 +0530

    previous day all work

--- event-management/src/main/resources/static/user/js/script.js
+++ event-management/src/main/resources/static/user/js/script.js
@@ -24,6 +24,38 @@ menu.addEventListener("click", () => {
   navbar.classList.toggle("open-menu");
 });
 
+// Service Dropdown js
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelector(".menu li");
+  const selected = dropdown.querySelector(".selected");
+
+  select.addEventListener("click", () => {
+    select.classList.toggle("select-clicked");
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      selected.innertext = option.innerText;
+      selected.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+
+      option.classList.add("active");
+    });
+  });
+});
+
 
 
 // navigation: {
