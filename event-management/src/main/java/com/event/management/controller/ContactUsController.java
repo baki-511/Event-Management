7f134457bb70e07315880e875b6cbf6b3f8537ec
baki-511 <johanbaki381@gmail.com>
2024-12-26 15:00:54 +0530

    Adding heading section to all

--- event-management/src/main/java/com/event/management/controller/ContactUsController.java
+++ event-management/src/main/java/com/event/management/controller/ContactUsController.java
@@ -2,6 +2,7 @@ package com.event.management.controller;
 
 import com.event.management.entity.ContactUs;
 import com.event.management.service.ContactUsService;
+import com.event.management.service.ServicesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +14,12 @@ public class ContactUsController {
     @Autowired
     private ContactUsService contactUsService;
     
+    @Autowired
+    private ServicesService servicesService;
+    
     @GetMapping("")
-    public String contactUs() {
+    public String contactUs(Model model) {
+        model.addAttribute("serviceList", servicesService.getAllServices());
         return "/pages/contact";
     }
    
